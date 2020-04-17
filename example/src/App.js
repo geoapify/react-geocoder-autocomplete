0bfbaec48ee799569cb4149f9194de59257b8db3
Alfiya Tarasenko <alfiya.tarasenko@geoapify.com>
2020-04-17 10:01:09 +0200

    Wrap Geocoder-Autocomplete with React component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,107 @@
-import React from 'react'
-
-import { ExampleComponent } from 'react-geocoder-autocomplete'
-import 'react-geocoder-autocomplete/dist/index.css'
+import React, { useState } from 'react'
+import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from 'react-geocoder-autocomplete'
+import '@geoapify/geocoder-autocomplete/styles/minimal.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  const [type, setType] = useState();
+  const [language, setLanguage] = useState();
+  const [position, setPosition] = useState();
+  const [countryCodes, setCountryCodes] = useState();
+  const [limit, setLimit] = useState();
+
+  function handleTypeChange(event) {
+    setType(event.target.value);
+  }
+
+  function handleLanguageChange(event) {
+    setLanguage(event.target.value);
+  }
+
+  function handleGeoLocationChange(event) {
+    const positionName = event.target.value;
+    if (positionName === 'Munich') {
+      setPosition({
+        lat: 48.140278,
+        lon: 11.562254
+      });
+    } else if (positionName === 'New York') {
+      setPosition({
+        lat: 40.716738,
+        lon: -74.001261
+      });
+    } else if (positionName === "Sydney") {
+      setPosition({
+        lat: -33.872866,
+        lon: 151.212336
+      });
+    }
+  }
+
+  function handleCountryCodesChange(event) {
+    setCountryCodes(event.target.value.split(','));
+  }
+
+  function handleLimitChange(event) {
+    setLimit(event.target.value);
+  }
+
+  function onPlaceSelect(value) {
+    console.log(value);
+  }
+
+  function onSuggectionChange(value) {
+    console.log(value);
+  }
+
+  return <div>
+    <div className="setting" onChange={handleTypeChange}>
+      <span className="label">Location type:</span>
+      <input type="radio" value="country" name="type" /> Country
+      <input type="radio" value="city" name="type" /> City
+      <input type="radio" value="amenity" name="type" /> Amenity
+    </div>
+
+    <div className="setting" onChange={handleLanguageChange}>
+      <span className="label">Language:</span>
+      <input type="radio" value="de" name="language" /> German
+      <input type="radio" value="it" name="language" /> Italian
+      <input type="radio" value="fr" name="language" /> French
+    </div>
+
+    <div className="setting" onChange={handleGeoLocationChange}>
+      <span className="label">Location bias:</span>
+      <input type="radio" value="Munich" name="position" /> Munich
+      <input type="radio" value="New York" name="position" /> New York
+      <input type="radio" value="Sydney" name="position" /> Sydney
+    </div>
+
+    <div className="setting" onChange={handleCountryCodesChange}>
+      <span className="label">Contries:</span>
+      <input type="radio" value="us" name="country" /> USA
+      <input type="radio" value="de,au" name="country" /> Germany and Austria
+      <input type="radio" value="ru,be" name="country" /> Russia and Belarus
+    </div>
+
+    <div className="setting" onChange={handleLimitChange}>
+      <span className="label">Limit:</span>
+      <input type="radio" value="2" name="limit" /> 2
+      <input type="radio" value="5" name="limit" /> 5
+      <input type="radio" value="10" name="limit" /> 10
+    </div>
+
+    <GeoapifyContext apiKey="00a9862ac01f454887fc285e220d8460">
+      <GeoapifyGeocoderAutocomplete placeholder="Enter address here"
+        type={type}
+        lang={language}
+        position={position}
+        countryCodes={countryCodes}
+        limit={limit}
+        placeSelect={onPlaceSelect}
+        suggestionsChange={onSuggectionChange}
+        />
+    </GeoapifyContext>
+  </div>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,94 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import React, { useEffect, useRef, MutableRefObject } from 'react';
+import { GeocoderAutocompleteOptions, GeocoderAutocomplete, LocationType, SupportedLanguage, GeoPosition, CountyCode } from '@geoapify/geocoder-autocomplete';
 
-interface Props {
-  text: string
+export const GeoapifyApiKey = React.createContext<string>('');
+
+export const GeoapifyContext = (props: any) => {
+  return <GeoapifyApiKey.Provider value={props.apiKey}>
+    {props.children}
+  </GeoapifyApiKey.Provider>
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export interface GeoapifyGeocoderAutocompleteOptions extends GeocoderAutocompleteOptions {
+  placeSelect: (value: any) => {},
+  suggestionsChange: (value: any) => {}
+}
+
+export const GeoapifyGeocoderAutocomplete = ({ placeholder: placeholderValue,
+  type: typeValue,
+  lang: langValue,
+  position: positionValue,
+  countryCodes: countryCodesValue,
+  limit: limitValue,
+  placeSelect: placeSelectCallback,
+  suggestionsChange: suggestionsChangeCallback}: GeoapifyGeocoderAutocompleteOptions) => {
+  const apiKey = React.useContext<string>(GeoapifyApiKey);
+  let geocoderContainer: HTMLDivElement | null;
+  let initialized: boolean = false;
+  let geocoderAutocomplete: MutableRefObject<GeocoderAutocomplete | undefined> = useRef<GeocoderAutocomplete>();
+
+  function onSelect(value: any) {
+    if (placeSelectCallback) {
+        placeSelectCallback(value);
+    }
+  }
+
+  function onSuggestions(value: any) {
+    if (suggestionsChangeCallback) {
+      suggestionsChangeCallback(value);
+    }
+  }
+
+  useEffect(() => {
+    if (initialized) {
+
+      if (geocoderAutocomplete.current) {
+        geocoderAutocomplete.current.off('select', onSelect);
+        geocoderAutocomplete.current.off('suggestions', onSuggestions);
+      }
+
+      return;
+    }
+
+    initialized = true;
+
+    geocoderAutocomplete.current = new GeocoderAutocomplete(geocoderContainer as HTMLDivElement, apiKey, {
+      placeholder: placeholderValue
+    });
+
+    geocoderAutocomplete.current.on('select', onSelect);
+    geocoderAutocomplete.current.on('suggestions', onSuggestions);
+  }, []);
+
+  useEffect(() => {
+    if (geocoderAutocomplete.current) {
+      geocoderAutocomplete.current.setType(typeValue as LocationType);
+    }
+  }, [typeValue]);
+
+  useEffect(() => {
+    if (geocoderAutocomplete.current) {
+      geocoderAutocomplete.current.setLang(langValue as SupportedLanguage);
+    }
+  }, [langValue]);
+
+  useEffect(() => {
+    if (geocoderAutocomplete.current) {
+      geocoderAutocomplete.current.setPosition(positionValue as GeoPosition);
+    }
+  }, [positionValue]);
+
+  useEffect(() => {
+    if (geocoderAutocomplete.current) {
+      geocoderAutocomplete.current.setCountryCodes(countryCodesValue as CountyCode[]);
+    }
+  }, [countryCodesValue]);
+
+  useEffect(() => {
+    if (geocoderAutocomplete.current) {
+      geocoderAutocomplete.current.setLimit(limitValue as number);
+    }
+  }, [limitValue]);
+
+  return <div className="geocoder-container" style={{ 'position': 'relative' }} ref={el => geocoderContainer = el}></div>
 }
