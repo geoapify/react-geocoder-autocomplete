439c2f18c52a6937f73299d559f2f46792cdf6d1
Oleg Hovsepyan <hovsepyan021@gmail.com>
2025-11-28 10:24:30 +0400

    GD-130: Update React-Geocoder-Autocomplete to support new options
    *Fixing the issue with dependencies (npm ci was failing)
    *For tests migrate react-scripts -> jest
    *For demo migrate react-scripts -> vite
    *Updating the Readme to match Angular's project

---
+++ example/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'build'
+  }
+});
+
